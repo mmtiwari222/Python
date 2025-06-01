51183dbf133f89924f2ab8edde12fa7a7287a88d
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-06-01 13:48:32 +0530

    Intro of literals in Python

---
+++ Basics/literals.py
@@ -0,0 +1,35 @@
+#literals => values that are directly assigned to a variable
+a = 0b1010 #binary literal
+b = 100 #decimal literal
+c = 0o310 #octal literal
+d = 0x12c #hexadecimal literal
+print(a, b, c, d)
+
+# string literals
+s1 = "Hello"
+s2 = 'World'
+print(s1, s2)
+
+# boolean literals
+b1 = True
+b2 = False
+print(b1, b2)
+
+# float literals
+f1 = 3.14
+f2 = 2.71828
+print(f1, f2)
+
+# complex literals
+c1 = 3 + 4j
+c2 = 5 + 6j
+print(c1, c2)
+print(c1.real, c1.imag)
+
+# bool 
+# True = 1
+# False = 0
+
+# None literal
+x = None
+print(x)
