cfd803918a9eaa92abe43a6e53f3938f8d997cf9
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-06-01 13:26:47 +0530

    take input from user and add the numbers

---
+++ Basics/addNumbers.py
@@ -0,0 +1,10 @@
+# take input from user and store it in variable
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# add both numbers and store it in another variable
+result = num1 + num2
+
+# print the result
+print("The sum of", num1, "and", num2, "is", result)
+print(type(num1))
