a14a02c1802afbaa175c5b19e7b48df639d9d17f
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-06-01 12:14:09 +0530

    Intro of variables in Python

---
+++ Basics/variables.py
@@ -0,0 +1,27 @@
+#variables => containers for storing data values
+#variables are used to store data values
+
+#variable naming rules =>
+#variable names cannot be a keyword
+#variable names cannot start with a number
+#variable names can only contain alpha-numeric characters, underscores and dollar sign
+
+name = "Madanmohan"
+print(name)
+
+num1 = 5
+num2 = 10
+print(num1 + num2)
+
+a, b, c = 2, 4, 6
+print(a, b, c)
+
+a = b = c = 10
+print(a, b, c)
+
+#Dynamic typing => where the type of the variable is determined at runtime by the value assigned to it is called dynamic typing
+#Static typing => where the type of the variable is determined at compile time means you need to specify the type of the variable when you declare it
+
+#Dynamic Binding => data type can be changed anytime in the program
+#Static Binding => data type cannot be changed once declared in the program
+
