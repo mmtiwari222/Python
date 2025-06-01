ade57432740ef24b2a4c7bd1f5144ab2cfea2fbc
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-06-01 11:37:32 +0530

    Print function in Python

---
+++ Basics/hello.py
@@ -0,0 +1,1 @@
+print("Hello World! It's my first python program")
---
+++ Basics/printFunction.py
@@ -0,0 +1,12 @@
+# print() => give the output in the console
+print("Hello world")
+print("Hello", 4, 7.8, True)
+print(2 + 2)
+
+#sep keyword = by default sep=" " (space)
+# separated by comma
+print("Hello", 4, 7.8, True, sep=",")
+
+# end keyword - by default end="\n" (new line)
+print("Hello", end="-")
+print("World")
