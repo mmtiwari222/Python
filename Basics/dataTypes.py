7a06473332c52b994c0fcbad1d4e4283596a3fe7
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-06-01 11:59:03 +0530

    Data types in Python

---
+++ Basics/dataTypes.py
@@ -0,0 +1,38 @@
+#Data types in Python
+# 1. String - text
+# 2. Integer - number
+# 3. Float - decimal
+# 4. Boolean - true or false
+# 5. complex - complex number
+# 6. list - list of items
+# 7. tuple - tuple of items
+# 8. set - set of items
+# 9. dictionary - collection of key-value pairs
+
+# Python is a case-sensitive programming language
+# Python is a dynamically typed programming language
+# Python is an interpreted programming language
+# Python is a multi-paradigm programming language
+# Python is a high-level programming language
+
+# examples of data types
+print(4)  # int
+print("Hello") # str
+print(3.14) # float
+print(True) # bool
+print(3 + 4j) # complex
+print([1, 2, 3]) #list
+print((1, 2, 3)) #tuple 
+print({1, 2, 3}) #set
+print({"name": "John", "age": 25}) #dict
+
+# type() => gives the data type of the variable
+print(type(4))
+print(type("Hello"))
+print(type(3.14))
+print(type(True))
+print(type(3 + 4j))
+print(type([1, 2, 3]))
+print(type((1, 2, 3)))
+print(type({1, 2, 3}))
+print(type({"name": "John", "age": 25}))
