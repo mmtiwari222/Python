7b6806062a2162d818ccd1f689dfeb4b990f72c0
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-11-24 18:04:18 +0000

    Intro of Python and All About Keywords and Varibales

--- Basics/intro.py
+++ Basics/intro.py
@@ -1,11 +1,13 @@
+# Program - Set of Instructions to perform a specific task
+# Program contains mainly two things - Data and Instructions
 # Introduction of Python -
 # Python is a high-level, interpreted, and general-purpose programming language. 
 # Python is developed by Guido van Rossum in 1989
 
 # Why Python --
-# 1. Design Philosophy - Python follows a clear, readable, and elegant design philosophy.
+# 1. Design Philosophy & Easy to Learn - Python follows a clear, readable, and elegant design philosophy.
 # 2. Battries Included - This phrase means Python comes with a rich standard library.
-# 3. Cross-platform (runs on Windows, macOS, Linux).
+# 3. Cross-platform or Platform Independant or Portable (runs on Windows, macOS, Linux).
 # 3. General Purpose - It can be used to build a wide variety of applications — not just web or mobile.
 # 4. Libraries/Community - Python has a massive ecosystem Plus, it has one of the largest and most supportive communities.
 
@@ -17,5 +19,5 @@
 # Python is a case-sensitive programming language
 # Python is a dynamically typed programming language
 # Python is an interpreted programming language
-# Python is a multi-paradigm programming language
+# Python is a multi-paradigm and general purpose programming language
 # Python is a high-level programming language
--- Basics/variables.py
+++ Basics/variables.py
@@ -1,27 +1,73 @@
 #variables => containers for storing data values
 #variables are used to store data values
+# Variable is a name given to data to identify it
+
+# In python varibles refer to memory location where data is stored
+# In python, variables are created when you assign a value to it - 
+# In python, you do not need to declare the variable type - Dynamic Typing
+
 
 #variable naming rules =>
-#variable names cannot be a keyword
-#variable names cannot start with a number
-#variable names can only contain alpha-numeric characters, underscores
-
+# Varibales name are case-sensitive. - 'name' and 'Name' are different variables
 name = "Madanmohan"
-print(name)
+Name = "Python"
+print(name, Name)
 
+# Variable should be meaningful - use names that convey the purpose of the variable (e.g., 'age' instead of 'a')
+age = 25 
+print(age)
+
+#variable names cannot be a keyword - reserved words in python like 'if', 'else', 'while', 'for', 'def', etc.
+# if = 10  # invalid
+valid_variable = 10  # valid
+print(valid_variable)
+
+#variable names cannot start with a number, it should start with a letter or underscore. - '1name' is invalid, '_name' or 'name1' is valid
+_name = "Madanmohan"
+name1 = "Python"
+print(_name, name1)
+
+#variable names can only contain alpha-numeric characters & One special character underscore. - 'name@' is invalid, 'name_' or 'name1' is valid (0-9, a-z, A-Z, and _ )
+name_ = "Madanmohan"
+name1 = "Python"
+print(name_, name1)
+#variable names cannot contain spaces. use underscore(_) instead of space
+first_name = "Madanmohan"
+print(first_name)
+
+# add two numbers using variables
 num1 = 5
 num2 = 10
 print(num1 + num2)
 
+# In python, variables can be assigned multiple values in a single line
+#multiple assignments in a single line
 a, b, c = 2, 4, 6
 print(a, b, c)
 
+# assigning same value to multiple variables in a single line
 a = b = c = 10
 print(a, b, c)
+# to check the memory address of the variables use id() function
+print(id(a), id(b), id(c))
+
 
 #Dynamic typing => where the type of the variable is determined at runtime by the value assigned to it is called dynamic typing
 #Static typing => where the type of the variable is determined at compile time means you need to specify the type of the variable when you declare it
 
 #Dynamic Binding => data type can be changed anytime in the program
 #Static Binding => data type cannot be changed once declared in the program
+
+# In python, variables can be reassigned to different data types
+var = 10        # integer
+print(var, type(var))
+var = "Python"  # string
+print(var, type(var))
+var = 10.5      # float
+print(var, type(var))
+var = True      # boolean
+print(var, type(var))
+
+
+
 
